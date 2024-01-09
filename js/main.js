ae5cc7ade872e93860dc9aa27262613475064769
bonvi83 <bonvi83@gmail.com>
2024-01-09 15:13:55 +0100

    add js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
-let age = parseInt ("Quanti hanni hai?")
+// Chiedo quanti km deve percorrere utente
+const km = prompt ("Quanti km devi percorrere?")
+console.log(km)
+
+// Chiedo quanti anni ha utente
+let age = prompt ("Quanti anni hai?")
 console.log(age)
