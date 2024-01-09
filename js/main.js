b6c97c258643bc53bcf9f471de3b6c878e6412bd
bonvi83 <bonvi83@gmail.com>
2024-01-09 16:19:30 +0100

    fixed error

--- js/main.js
+++ js/main.js
@@ -1,29 +1,33 @@
 // Chiedo quanti km deve percorrere utente
-const km = prompt ("Quanti km devi percorrere?")
+const km = prompt ("Quanti km devi percorrere?");
 console.log(km)
 
 // Chiedo quanti anni ha utente
-const age = prompt ("Quanti anni hai?")
+const age = prompt ("Quanti anni hai?");
 console.log(age)
 
+
 //Il calcolo parziale del biglietto
-const parzialTicket = km * age
-console.log(parziale)
+const parzialTicket = km * 0.21;
+console.log(parzialTicket)
 
 
 //Applico sconto se rispetta alcuni parametri
-let sconto20 = parzialTicket * 0.2
-let sconto40 = parzialTicket * 0.4
+let sconto20 = parzialTicket * 0.2;
+let sconto40 = parzialTicket * 0.4;
 
+
+let priceTicket = 0;
 
 //Calcolo del prezzo del biglietto
 if (age < 18) {
-    let priceTicket = parzialTicket - sconto20;
+    priceTicket = parzialTicket - sconto20;
 } else if (age > 65) {
-    let priceTicket = parzialTicket - sconto40;
+    priceTicket = parzialTicket - sconto40;
 } else {
-    let priceTicket = priceTicket
+    priceTicket = parzialTicket;
 }
 
+
 //Comunico all'utente la cifra
-document.getElementById ("prezzo").innerHTML = priceTicket
+document.getElementById ("prezzo").innerHTML = priceTicket;
